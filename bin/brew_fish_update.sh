e452dcc9b6e4e14876dcd7aa67fc8414e31a70b3
liara <liara@users.noreply.github.com>
2018-09-02 14:39:41 +0200

    :apple:

--- bin/brew_fish_update.sh
+++ bin/brew_fish_update.sh
@@ -7,14 +7,12 @@ brew update
 brew upgrade
 brew cleanup
 brew doctor
-echo 'Ignore greenlet warnings. Had to be installed for neovim python.'
+echo 'ignore greenlet warnings. had to be installed for neovim'
 brew cask upgrade
 brew cask cleanup
 
 echo 'updating fish shell'
 fisher up
-
-# modify the prompt, make it a fish
 tail -n 2 "$HOME/.config/fish/functions/fish_prompt.fish" | wc -c | xargs -I {} gtruncate "$HOME/.config/fish/functions/fish_prompt.fish" -s -{}
 cat $HOME/.config/fish/functions/alter_prompt.txt >> $HOME/.config/fish/functions/fish_prompt.fish
 fish_update_completions
@@ -22,10 +20,10 @@ fish_update_completions
 echo 'updating python and pip pkgs'
 python -m pip install --upgrade pip setuptools wheel
 pip list --outdated
-pip list --outdated | awk '{print $1}' | xargs -n1 -p sudo -H pip install --upgrade
+pip list --outdated | awk '{print $1}' | xargs -n1 -p pip install --upgrade
 python3 -m pip install --upgrade pip setuptools wheel
 pip3 list --outdated
-pip3 list --outdated | awk '{print $1}' | xargs -n1 -p sudo -H pip install --upgrade
+pip3 list --outdated | awk '{print $1}' | xargs -n1 pip3 install -U
 
 #echo 'updating npm'
 #npm update -g
